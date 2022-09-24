da38c8899402ba78c5632dd9e73e192a37f23481
Lucas <lucas.santos@mundowap.com.br>
2022-09-24 11:01:09 -0300

    chore: add eslint and .editorconfig

--- src/index.js
+++ src/index.js
@@ -2,9 +2,6 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', (request, response) => response.send('<h1> Teste </h1>'));
 
-app.get('/', (request, response) =>{
-  return response.send(`<h1> Teste </h1>`);
-})
-
-app.listen(3001, () => console.log(`Server is runing at http://localhost:3001`))
+app.listen(3001, () => console.log('Server is runing at http://localhost:3001'));
